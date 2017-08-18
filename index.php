659203b838a6acf3b85e97c6551e4cda71d34371
Eric Moss <ericbrianmoss@gmail.com>
2017-08-17 22:13:46 -0400

    Fixed an error that was generated in index.php

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ get_header(); ?>
       <div class="entries">
         <?php
         if ( have_posts() ) :
-          $i = 0;
           while ( have_posts() ) :
         		the_post(); ?>
             <article <?php post_class('entry'); ?>>
@@ -40,7 +39,6 @@ get_header(); ?>
                     <a href="<?php the_permalink(); ?>" class="entry-image-placeholder"></a>
                   <?php
                   endif; ?>
-<<<<<<< HEAD
                 </div>
                 <div class="card-body">
                   <a href="<?php the_permalink(); ?>">
@@ -55,42 +53,6 @@ get_header(); ?>
                     <small class="entry-meta">
                       <span class="entry-date"><?php echo get_the_date(); ?></span>
                       <span class="entry-author"><?php the_author_posts_link(); ?></span>
-=======
-                  <article <?php post_class('row entry'); ?>>
-                    <div class="col-md-7 <?php echo $col_image_classes; ?> entry-image">
-                      <?php
-                      if ( has_post_thumbnail() ) :
-                        echo '<a href="' . get_the_permalink() . '">';
-                        the_post_thumbnail('loop', array(
-                          'class' => 'img-fluid',
-                        ));
-                        echo '</a>';
-                      else: ?>
-                        <a href="<?php the_permalink(); ?>" class="entry-image-placeholder"></a>
-                      <?php
-                      endif; ?>
-                    </div>
-                    <div class="col-md-5 <?php echo $col_content_classes; ?> entry-details">
-                      <a href="<?php the_permalink(); ?>">
-                        <h3 class="entry-title"><?php the_title(); ?></h3>
-                      </a>
-                      <?php
-                      if ( 'post' == get_post_type() ) : ?>
-                        <ul class="entry-meta">
-                          <li class="entry-date"><?php echo get_the_date(); ?></li>
-                          <li class="entry-author">Author: <?php the_author_posts_link(); ?></li>
-                          <?php
-                          $category_list = array();
-                          if ( has_category() ) :
-                            echo '<li class="entry-categories">Category: ';
-                            foreach ( get_the_category() as $category) :
-                              $category_list[] = '<a href="'. get_category_link( $category->term_id ) .'">' . $category->cat_name . '</a>';
-                            endforeach;
-                            echo implode(', ', $category_list);
-                            echo '</li>';
-                          endif;?>
-                        </ul>
->>>>>>> origin/master
                       <?php
                       $category_list = array();
                       if ( has_category() ) :
@@ -108,17 +70,15 @@ get_header(); ?>
               </div>
             </article>
           <?php
-          $i++;
         	endwhile; ?>
           <div class="pagination-container">
-            <?php
-            the_posts_pagination(); ?>
+            <?php the_posts_pagination(); ?>
           </div>
           <?php
+          wp_reset_postdata();
         else :
           echo '<div class="alert alert-warning">Oops! No posts were found.</div>';
-        endif;
-        wp_reset_postdata(); ?>
+        endif; ?>        
       </div>
 
     </main>
