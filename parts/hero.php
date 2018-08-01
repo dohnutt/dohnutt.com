bed90f7a70bc891fff647fe9973726da667b9beb
Eric Moss <ericbrianmoss@gmail.com>
2018-08-01 00:06:17 -0400

    Remove fluff from hero template, and switch to using my new title helper function

--- parts/hero.php
+++ parts/hero.php
@@ -1,29 +1,3 @@
-<header class="hero">
-  <?php
-  if ( function_exists('yoast_breadcrumb') )
-    yoast_breadcrumb('<p class="breadcrumbs">','</p>');
-
-  $title = get_the_title();
-  $title_alt = get_field('title_alt');
-
-  if ( $title_alt )
-    $title = $title_alt;
-
-  if ( is_post_type_archive('portfolio') ) :
-    echo '<h1 class="entry-title">Portfolio</h1>';
-
-  elseif ( is_home() ) :
-    echo '<h1 class="entry-title">Thoughts</h1>';
-
-  elseif ( is_archive() ) :
-    the_archive_title('<h1 class="entry-title">','</h1>');
-
-  elseif ( is_search() ) :
-    echo '<h1 class="entry-title"><em>Search:</em> ' . get_search_query() . '</h1>';
-
-  else :
-    echo '<h1 class="entry-title">' . $title . '</h1>';
-    
-  endif;
-  ?>
-</header>
+<section class="pg-hero">
+    <h1 class="pg-title"><?php echo get_the_right_title(); ?></h1>
+</section>
