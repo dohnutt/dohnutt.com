e78b21fedc91446876ab114af91aee480dd7adf3
Eric Moss <ericbrianmoss@gmail.com>
2020-09-13 10:36:14 -0400

    Disable emojis in WordPress

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -93,6 +93,41 @@ if ( ! function_exists('doh_theme_enqueue') ) {
 
 
 
+// Disable all emoji libraries from WordPress
+add_action( 'init', 'doh_disable_emojis' );
+function disable_emojis() {
+  
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+  add_filter( 'tiny_mce_plugins', 'doh_disable_emojis_tinymce' );
+  add_filter( 'wp_resource_hints', 'doh_disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+
+// Filter funcion to remove the emoji plugin from TinyMCE.
+function doh_disable_emojis_tinymce($plugins) {
+  if ( is_array( $plugins ) ) {
+    return array_diff( $plugins, array( 'wpemoji' ) );
+  } else return array();
+}
+
+// Removing emoji CDN hostname from DNS prefetching hints
+function doh_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+  if ( 'dns-prefetch' == $relation_type ) {
+    // This filter is documented in wp-includes/formatting.php
+    $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+    $urls = array_diff( $urls, array( $emoji_svg_url ) );
+  }
+  return $urls;
+}
+
+
+
 // Add theme supports and nav menus.
 if ( !function_exists('doh_theme_support') ) {
   add_action('after_setup_theme', 'doh_theme_support');
