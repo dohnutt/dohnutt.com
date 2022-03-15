ce3c0b406bc1aed07c30a6717f3c332c3539954b
Eric Moss <ericbrianmoss@gmail.com>
2022-03-14 23:29:18 -0400

    Add theme support for `wp-block-styles`

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -119,6 +119,7 @@ if ( !function_exists('doh_theme_support') ) {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption') );
+    add_theme_support( 'wp-block-styles' );
     add_theme_support( 'responsive-embeds' );
     add_theme_support( 'automatic-feed-links' );
     //add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
