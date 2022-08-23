23422c755b8882af391c96be4835b4654ec75e1f
Eric Moss <ericbrianmoss@gmail.com>
2022-08-23 19:23:26 -0400

    Rework build process

---
+++ _src/js/app.js
@@ -0,0 +1,145 @@
+import "../scss/style.scss";
+
+//import "bootstrap/js/dist/util.js";
+//import "bootstrap/js/dist/alert.js";
+//import "bootstrap/js/dist/button.js";
+//import "bootstrap/js/dist/collapse.js";
+//import "bootstrap/js/dist/dropdown.js";
+//import "bootstrap/js/dist/modal.js";
+//import "bootstrap/js/dist/scrollspy.js";
+//import "bootstrap/js/dist/tab.js";
+//import "bootstrap/js/dist/index.js";
+
+
+document.documentElement.classList.remove('no-js');
+
+
+/**
+ * Light/dark mode
+ */
+
+if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+	console.log('🎉 Dark mode is supported');
+}
+
+const STORAGE_KEY = 'user-color-scheme';
+const COLOR_MODE_KEY = '--color-mode';
+const schemeToggleButton = document.querySelector('.js-scheme-toggle');
+const schemeLabel = document.querySelector('.js-scheme-label');
+
+const getCSSCustomProp = function (propKey) {
+	let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+
+	if (response.length) {
+		response = response.replace(/[\"\']/g, '').trim();
+	}
+
+	return response;
+};
+
+const applySetting = function (passedSetting) {
+	let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY) || getCSSCustomProp(COLOR_MODE_KEY);
+
+	document.documentElement.classList.remove('light-mode', 'dark-mode');
+	document.documentElement.classList.add(currentSetting + '-mode');
+	setToggle(currentSetting);
+};
+
+const setToggle = function (currentSetting) {
+	let checkedVal = currentSetting === 'dark' ? true : false;
+	let label = currentSetting === 'dark' ? '🌙' : '☀';
+	let ariaLabel = currentSetting === 'dark' ? 'Dark mode' : 'Light mode';
+	schemeToggleButton.checked = checkedVal;
+	schemeLabel.innerText = label;
+	schemeLabel.setAttribute('aria-label', ariaLabel);
+};
+
+const toggleSetting = function () {
+	let currentSetting = localStorage.getItem(STORAGE_KEY);
+
+	switch (currentSetting) {
+		case null:
+			currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+			break;
+		case 'light':
+			currentSetting = 'dark';
+			break;
+		case 'dark':
+			currentSetting = 'light';
+			break;
+	}
+
+	localStorage.setItem(STORAGE_KEY, currentSetting);
+
+	return currentSetting;
+};
+
+schemeToggleButton.addEventListener('click', function (event) {
+	applySetting(toggleSetting());
+});
+
+applySetting();
+
+
+
+/*
+ * Wrap all em and en dashes in `span.resize-dash`
+ */
+function wrapDashes() {
+	var content = document.querySelector('.entry__content');
+	content.innerHTML = content.innerHTML.replace(/\b(—|–|&ndash;|&mdash;)/g, '<span class="resize-dash">$1</span>')
+}
+
+wrapDashes();
+
+
+
+/*
+ * Toggle navbar
+ */
+$(document).click(function (event) {
+	var _opened = $('.navbar-collapse').hasClass('show');
+
+	if (!$(event.target).closest('.navbar-collapse').length && !$(event.target).is('.navbar-collapse') && _opened === true) {
+		$('.navbar-collapse').collapse('toggle');
+	}
+});
+
+
+
+/*
+ * Accessible skip to content link
+ */
+document.querySelector('.js-a11y-skip').addEventListener('click', function (event) {
+
+	event.preventDefault();
+
+	var el = document.querySelector('#' + this.href.split('#')[1]);
+	var removeElTabindex = function () {
+		el.removeAttribute('tabindex');
+	}
+
+	el.setAttribute('tabindex', '-1');
+	el.addEventListener('blur', removeElTabindex, false);
+	el.addEventListener('focusout', removeElTabindex, false);
+	el.focus();
+
+});
+
+
+
+/*
+ * Open share links in a small popup window
+ */
+$('.share__link').click(function (event) {
+
+	event.preventDefault();
+
+	window.open(
+		$(this).attr('href'),
+		'shareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0'
+	);
+
+	return false;
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,47 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './assets/src/js/app.js',
+    entry: './_src/js/app.js',
     output: {
         filename: 'app.min.js',
-        path: path.resolve(__dirname, 'assets/js')
+        path: path.resolve(__dirname, 'js')
     },
     optimization: {
         mangleExports: true,
+    },
+    target: 'browserslist',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { url: false }
+                    },
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: '../style.css' })
+    ],
+    externals: {
+        // jQuery is external and available via the global var `jQuery`
+        "jquery": "jQuery"
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
 };
