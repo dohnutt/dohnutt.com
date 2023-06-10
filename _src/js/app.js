aa7109d13f19fe70c9103c0fa144efa3627a2a79
Eric Moss <ericbrianmoss@gmail.com>
2023-06-10 10:22:44 -0400

    Rename "theme" to "scheme"

--- _src/js/app.js
+++ _src/js/app.js
@@ -2,36 +2,36 @@ import "../scss/style.scss";
 
 
 /**
- * Light/dark mode
+ * Colour schemer
  */
 
 if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
 	console.log('🎉 Dark mode is supported');
 }
 
-const STORAGE_KEY = 'doh_theme';
-const themeToggleButton = document.querySelector('.js-theme-toggle > a');
-const themes = ['pink-light', 'pink-dark', 'teal-light', 'teal-dark'];
+const STORAGE_KEY = 'doh_scheme';
+const schemeToggleButton = document.querySelector('.js-scheme-toggle > a');
+const schemes = ['pink-light', 'pink-dark', 'teal-light', 'teal-dark'];
 
 function applySetting(passedSetting) {
-	let currentTheme = passedSetting || window.localStorage.getItem(STORAGE_KEY) || themes[0];
+	let currentScheme = passedSetting || window.localStorage.getItem(STORAGE_KEY) || schemes[0];
 
-	document.body.removeAttribute('data-theme');
-	document.body.setAttribute('data-theme', currentTheme);
+	document.body.removeAttribute('data-scheme');
+	document.body.setAttribute('data-scheme', currentScheme);
 }
 
 function toggleSetting() {
-	let currentTheme = window.localStorage.getItem(STORAGE_KEY) || themes[0];
-	let currentThemeIndex = themes.indexOf(currentTheme) || 0;
+	let currentScheme = window.localStorage.getItem(STORAGE_KEY) || schemes[0];
+	let currentSchemeIndex = schemes.indexOf(currentScheme) || 0;
 
-	currentTheme = currentThemeIndex >= (themes.length - 1) ? themes[0] : themes[currentThemeIndex + 1];
-	window.localStorage.setItem(STORAGE_KEY, currentTheme);
-	console.log('😎 Theme changed to ' + currentTheme);
+	currentScheme = currentSchemeIndex >= (schemes.length - 1) ? schemes[0] : schemes[currentSchemeIndex + 1];
+	window.localStorage.setItem(STORAGE_KEY, currentScheme);
+	console.log('😎 Scheme changed to ' + currentScheme);
 	
-	return currentTheme;
+	return currentScheme;
 }
 
-themeToggleButton.addEventListener('click', function (event) {
+schemeToggleButton.addEventListener('click', function (event) {
 	event.preventDefault();
 	applySetting(toggleSetting());
 });
